8b12572287050560b5d09a2f0071ca3fcfb5e409
dipes <dipeshkhatri428@gmail.com>
2025-12-10 06:51:09 +0545

    Revert "add conslole from dipesh"
    This reverts commit 36de63c87a3e718f193b055d516db325df492d14.

--- hello.js
+++ hello.js
@@ -2,7 +2,10 @@ console.log("hello, git");
 
 console.log ("hello, gitbub engineer");
 
+<<<<<<< HEAD
 console.log( "hello, from dipesh engineer");
 
 
 console.log( "dipesh engineer engineer ");
+=======
+>>>>>>>
